9c0ce293b5e8f1eec5ad4bade09070f83dddc173
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-09-21 12:06:22 +0600

    main

--- lesson10/index.js
+++ lesson10/index.js
@@ -2,7 +2,25 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 app.get("/products/:id([0-9]+)", (req, res) =>{
-    res.send(`<h1>ID = ${req.params.id}</h1>`)
+    res.status(200).send({
+        message: `The Product id is ${req.params.id}`,
+        status: 200
+    })
+})
+
+app.get("/products/:title([a-zA-Z]+)", (req,res) =>{
+    res.send(
+        {
+            message: `The product title is ${req.params.title}`,
+            status: 200
+        }
+    )
+})
+
+app.use("*", (req,res)=>{
+    res.status(404).send({
+        message: "Not a Valid Route"
+    })
 })
 
 app.listen(PORT, ()=>{
